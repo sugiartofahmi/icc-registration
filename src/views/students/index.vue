ce616a51705511b6c6c5084480ec238aa40183c3
Fahmi <sugiartofahmi@gmail.com>
2022-06-16 09:27:37 +0700

    Change text color

--- src/views/students/index.vue
+++ src/views/students/index.vue
@@ -136,7 +136,7 @@ onMounted(async () => {
                       NIM
                     </th>
                     <th
-                      class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider"
+                      class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-black uppercase tracking-wider"
                     >
                       Angkatan
                     </th>
